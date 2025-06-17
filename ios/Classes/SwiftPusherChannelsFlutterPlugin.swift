6bcb03361d5c7bb59d29a33ea3dd12d04ac97639
MarioOtmacic <mario.otmacic@3ofthem.eu>
2025-06-17 22:35:30 +0200

    patched version,  fix the iOS threading issue

--- ios/Classes/SwiftPusherChannelsFlutterPlugin.swift
+++ ios/Classes/SwiftPusherChannelsFlutterPlugin.swift
@@ -117,12 +117,14 @@ public class SwiftPusherChannelsFlutterPlugin: NSObject, FlutterPlugin, PusherDe
     }
   }
 
-  public func changedConnectionState(from old: ConnectionState, to new: ConnectionState) {
-    methodChannel.invokeMethod("onConnectionStateChange", arguments: [
+public func changedConnectionState(from old: ConnectionState, to new: ConnectionState) {
+  DispatchQueue.main.async {
+    self.methodChannel.invokeMethod("onConnectionStateChange", arguments: [
       "previousState": old.stringValue(),
       "currentState": new.stringValue(),
     ])
   }
+}
 
   public func debugLog(message _: String) {
     // print("DEBUG:", message)
@@ -132,17 +134,21 @@ public class SwiftPusherChannelsFlutterPlugin: NSObject, FlutterPlugin, PusherDe
     // Handled by global handler
   }
 
-  public func failedToSubscribeToChannel(name _: String, response _: URLResponse?, data _: String?, error: NSError?) {
-    methodChannel.invokeMethod(
-      "onSubscriptionError", arguments: [
-        "message": (error != nil) ? error!.localizedDescription : "",
-        "error": error.debugDescription,
-      ]
-    )
-  }
+ public func failedToSubscribeToChannel(name _: String, response _: URLResponse?, data _: String?, error: NSError?) {
+   DispatchQueue.main.async {
+     self.methodChannel.invokeMethod(
+       "onSubscriptionError", arguments: [
+         "message": (error != nil) ? error!.localizedDescription : "",
+         "error": error.debugDescription,
+       ]
+     )
+   }
+ }
 
-  public func receivedError(error: PusherError) {
-    methodChannel.invokeMethod(
+
+public func receivedError(error: PusherError) {
+  DispatchQueue.main.async {
+    self.methodChannel.invokeMethod(
       "onError", arguments: [
         "message": error.message,
         "code": error.code ?? -1,
@@ -150,15 +156,18 @@ public class SwiftPusherChannelsFlutterPlugin: NSObject, FlutterPlugin, PusherDe
       ]
     )
   }
+}
 
-  public func failedToDecryptEvent(eventName: String, channelName _: String, data: String?) {
-    methodChannel.invokeMethod(
-      "onDecryptionFailure", arguments: [
-        "eventName": eventName,
-        "reason": data,
-      ]
-    )
-  }
+ public func failedToDecryptEvent(eventName: String, channelName _: String, data: String?) {
+   DispatchQueue.main.async {
+     self.methodChannel.invokeMethod(
+       "onDecryptionFailure", arguments: [
+         "eventName": eventName,
+         "reason": data,
+       ]
+     )
+   }
+ }
 
   func connect(result: @escaping FlutterResult) {
     pusher.connect()
@@ -174,14 +183,15 @@ public class SwiftPusherChannelsFlutterPlugin: NSObject, FlutterPlugin, PusherDe
     result(pusher.connection.socketId)
   }
 
-  func onEvent(event: PusherEvent) {
-    var userId: String?
-    if event.eventName == "pusher:subscription_succeeded" {
-      if let channel = pusher.connection.channels.findPresence(name: event.channelName!) {
-        userId = channel.myId
-      }
+func onEvent(event: PusherEvent) {
+  var userId: String?
+  if event.eventName == "pusher:subscription_succeeded" {
+    if let channel = pusher.connection.channels.findPresence(name: event.channelName!) {
+      userId = channel.myId
     }
-    methodChannel.invokeMethod(
+  }
+  DispatchQueue.main.async {
+    self.methodChannel.invokeMethod(
       "onEvent", arguments: [
         "channelName": event.channelName,
         "eventName": event.eventName,
@@ -190,30 +200,36 @@ public class SwiftPusherChannelsFlutterPlugin: NSObject, FlutterPlugin, PusherDe
       ]
     )
   }
+}
 
-  func subscribe(call: FlutterMethodCall, result: @escaping FlutterResult) {
-    let args = call.arguments as! [String: String]
-    let channelName: String = args["channelName"]!
-    if channelName.hasPrefix("presence-") {
-      let onMemberAdded: (PusherPresenceChannelMember) -> Void = { user in
+func subscribe(call: FlutterMethodCall, result: @escaping FlutterResult) {
+  let args = call.arguments as! [String: String]
+  let channelName: String = args["channelName"]!
+  if channelName.hasPrefix("presence-") {
+    let onMemberAdded: (PusherPresenceChannelMember) -> Void = { user in
+      DispatchQueue.main.async {
         self.methodChannel.invokeMethod("onMemberAdded", arguments: [
           "channelName": channelName,
           "user": ["userId": user.userId, "userInfo": user.userInfo],
         ])
       }
-      let onMemberRemoved: (PusherPresenceChannelMember) -> Void = { user in
+    }
+    let onMemberRemoved: (PusherPresenceChannelMember) -> Void = { user in
+      DispatchQueue.main.async {
         self.methodChannel.invokeMethod("onMemberRemoved", arguments: [
           "channelName": channelName,
           "user": ["userId": user.userId, "userInfo": user.userInfo],
         ])
       }
-      pusher.subscribeToPresenceChannel(
-        channelName: channelName,
-        onMemberAdded: onMemberAdded,
-        onMemberRemoved: onMemberRemoved
-      )
-    } else {
-      let onSubscriptionCount: (Int) -> Void = { subscriptionCount in
+    }
+    pusher.subscribeToPresenceChannel(
+      channelName: channelName,
+      onMemberAdded: onMemberAdded,
+      onMemberRemoved: onMemberRemoved
+    )
+  } else {
+    let onSubscriptionCount: (Int) -> Void = { subscriptionCount in
+      DispatchQueue.main.async {
         self.methodChannel.invokeMethod(
           "onEvent", arguments: [
             "channelName": channelName,
@@ -225,11 +241,12 @@ public class SwiftPusherChannelsFlutterPlugin: NSObject, FlutterPlugin, PusherDe
           ]
         )
       }
-      pusher.subscribe(channelName: channelName,
-                       onSubscriptionCountChanged: onSubscriptionCount)
     }
-    result(nil)
+    pusher.subscribe(channelName: channelName,
+                     onSubscriptionCountChanged: onSubscriptionCount)
   }
+  result(nil)
+}
 
   func unsubscribe(call: FlutterMethodCall, result: @escaping FlutterResult) {
     let args = call.arguments as! [String: String]
